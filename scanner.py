936dd635ad7a771e67ec8063d69359fe5fabff69
David Gillis <dgillis91@gmail.com>
2020-04-22 10:28:03 -0500

    add support for iteration

--- scanner.py
+++ scanner.py
@@ -13,6 +13,9 @@ class Scanner:
         self._start_state = None
         self._end_states = set()
 
+        self._identifier_iterator = 0
+        self._generator = self._reader.get_char()
+
     def add_state(self, name, handler, end_state=False):
         self._handlers[name] = handler
         if end_state:
@@ -20,11 +23,6 @@ class Scanner:
         
     def set_start(self, name):
         self._start_state = name
-
-    def run(self):
-        buffer = list()
-        for character in self._reader.get_char():
-            buffer.append(character)
 
     def _reset_fsm(self):
         try:
@@ -35,18 +33,21 @@ class Scanner:
             raise RuntimeError('at least one state must be an end_state')
         return handler
 
-    def get_token(self):
+    def __iter__(self):
+        self._identifier_iterator = 0
+        return self
+
+    def __next__(self):
         handler = self._reset_fsm()
         buffer = list()
-        identifier = 0
-
-        for char in self._reader.get_char():
+        while True:
+            char = next(self._generator)
+            if char.character == '':
+                return Token('keyword', 'eof', self._identifier_iterator, char.line_number)
             buffer.append(char.character)
             new_state = handler(char.character)
             if new_state in self._end_states:
-                yield Token(new_state, ''.join(buffer).strip(), identifier, char.line_number)
-                buffer = list()
-                identifier += 1
-                handler = self._reset_fsm()
+                self._identifier_iterator += 1
+                return Token(new_state, ''.join(buffer).strip(), self._identifier_iterator, char.line_number)
             else:
                 handler = self._handlers[new_state]
