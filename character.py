afb40bddfc671807c364b64c81c8e76d44a192f9
David Gillis <dgillis91@gmail.com>
2020-04-22 09:40:35 -0500

    add str methods

--- character.py
+++ character.py
@@ -6,3 +6,9 @@ class Character:
         self.character = character
         self.character_index = character_index
         self.line_number = line_number
+
+    def __repr__(self):
+        return '< {} >'.format(self.__str__())
+
+    def __str__(self):
+        return 'character: {}; character_index {}; line_number: {};'.format(self.character, self.character_index, self.line_number)
